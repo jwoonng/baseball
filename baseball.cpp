cc8b9772259178a6ddfa0b425aa442245509a97b
jw0825.lim <iju825@naver.com>
2026-05-28 13:38:25 +0900

    3letter error exception

--- baseball.cpp
+++ baseball.cpp
@@ -1,4 +1,12 @@
-class Baseball {
+#include <stdexcept>
+#include <string>
 
+using namespace std;
+
+class Baseball {
+public:
+	void guess(const string& string) {
+		throw length_error("Must be three letters");
+	}
 
 };
--- baseball_test.cpp
+++ baseball_test.cpp
@@ -1,2 +1,9 @@
 #include "baseball.cpp"
 #include "gmock/gmock.h"
+#include "baseball_test.h"
+
+
+TEST(baseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
