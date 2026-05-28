676f3758c46c8ceaa8427f2521d22a7ee3118fdd
jw0825.lim <iju825@naver.com>
2026-05-28 13:46:56 +0900

    invalid case refactor

--- baseball_test.cpp
+++ baseball_test.cpp
@@ -2,13 +2,22 @@
 #include "gmock/gmock.h"
 #include "baseball_test.h"
 
-
-TEST(baseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
+	void assertIllegalArgument(string guessNumber) {
+		Baseball game;
+		try {
+			game.guess(string(guessNumber));
+			FAIL();
+		}
+		catch (exception e) {
+			//EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+		}
+	}
+};
 
-TEST(baseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
