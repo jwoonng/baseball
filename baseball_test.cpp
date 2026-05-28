53fd6d947ac453328f7c44b30ee7386783515085
jw0825.lim <iju825@naver.com>
2026-05-28 14:05:54 +0900

    strikes

--- baseball_test.cpp
+++ baseball_test.cpp
@@ -30,3 +30,11 @@ TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
 	EXPECT_EQ(3, result.strikes);
 	EXPECT_EQ(0, result.balls);
 }
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfBallNumber) {
+	GuessResult result = game.guess("124");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(2, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
